9dd7cb94f670a96c575f9f39de10cd371e261596
242427 Michał Korblit <242427@edu.p.lodz.pl>
2023-04-12 10:10:37 +0200

    implementation of circle class and AbstractApiModel class

--- Etap1/Etap1/Presentation/Model/AbstractAPIModel.cs
+++ Etap1/Etap1/Presentation/Model/AbstractAPIModel.cs
@@ -6,7 +6,77 @@ using System.Threading.Tasks;
 
 namespace Etap1.Presentation.Model
 {
-    class Model
-    {
+    public abstract class Model {
+
+        public static AbstractAPIModel abstractAPIModel(AbstractLogicApi abstractLogicApi = null)
+        {
+            return new ModelApi();
+        }
+
+        public abstract void CreateScene(int ballQuantity, int ballRadius);
+        public abstract ObservableCollection<Circle> GetAllCircles();
+        public abstract void Enable();
+        public abstract void Disable();
+        public abstract bool IsEnabled();
+        public sealed class ModelApi : AbstractAPIModel
+
+            //zmiany poniżej
+        {
+            private AbstractLogicApi logicApi = AbstractLogicApi.CreateApi(null);
+            private ObservableCollection<Circle> circles = new ObservableCollection<Circle>();
+            public ObservableCollection<Circle> Circles
+            {
+                get
+                {
+                    return circles;
+                }
+                set
+                {
+                    circles = value;
+                }
+            }
+        }
+
+        public ModelApi (AbstractLogicApi abstractLogicApi = null)
+        {
+            if( abstractLogicApi = null)
+            {
+                this.logicApi = abstractLogicApi.CreateApi();
+            }
+            else
+            {
+                this.logicApi = abstractLogicApi;
+            }
+        }
+
+        public override void CreateScene(int ballQuantity, int ballRadius)
+        {
+            logicApi.CreateScene(750, 600, ballQuantity, ballRadius);
+        }
+
+        public override ObservableCollection<Circle> GetAllCircles()
+        {
+            List<Orb> orbs = logicApi.GetOrbs();
+            Circles.Clear();
+            foreach (Orb orb in orbs)
+            {
+                Circles.Add(new Circle(orb));
+            }
+            return Circles;
+        }
+
+        public override void Enable()
+        {
+            logicApi.Enable();
+        }
+
+        public override void Disable()
+        {
+            logicApi.Disable();
+        }
+
+        public override bool IsEnabled()
+        {
+            return logicApi.IsEnabled();
+        }
     }
-}
--- Etap1/Etap1/Presentation/Model/Circle.cs
+++ Etap1/Etap1/Presentation/Model/Circle.cs
@@ -1,12 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Logika;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Etap1.Presentation.Model
 {
-    class Model
+    public class Circle : INotifyPropertyChanged
     {
+        private int x;
+        private int y;
+        private int radius;
+    }
+
+    public Circle(Ball ball)
+    {
+        this.x = ball.X;
+        this.y = ball.Y;
+        this.radius = ball.Radius;
+        ball.PropertyChanged += Update;
+    }
+
+    private void Update(object source, PropertyChangedEventArgs key)
+    {
+        Ball sourceBall = (Ball)source;
+        if (key.PropertyName == "X")
+        {
+            this.x = sourceBall.X - sourceBall.Radius;
+        }
+        if (key.PropertyName == "Y")
+        {
+            this.y = sourceBall.Y - sourceBall.Radius;
+        }
+        if (key.PropertyName == "Radius")
+        {
+            this.radius = sourceBall.Radius;
+        }
+
+         public int X
+    {
+        get { return x; }
+        set
+        {
+            x = value;
+            OnPropertyChanged("X");
+        }
+    }
+
+    public int Y
+    {
+        get { return y; }
+        set
+        {
+            y = value;
+            OnPropertyChanged("Y");
+        }
+    }
+
+    public int Radius
+    {
+        get { return radius; }
+        set
+        {
+            radius = value;
+            OnPropertyChanged("Radius");
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
